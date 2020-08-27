7be1b7604d56b6a13002ed84ae17a6bd78983e4e
brad <bradley.wilsher@infinityworks.com>
2020-08-27 17:34:22 +0100

    init 2d array

--- grid.js
+++ grid.js
@@ -1,6 +1,19 @@
 const rows = 3;
 const cols =3;
 
+
+
+// let board = new Array(cols);
+// for (let i =0; i < rows; i++) {
+//     board[i] = new Array(rows);
+// }
+
+//Initalise an empty 2d array
+let board = Array(cols).fill().map(() => Array(rows));
+console.log(board);
+
+
+
 for (let i =0; i < rows; i++) {
   const row = document.createElement("div");
   document.body.appendChild(row);
@@ -18,28 +31,7 @@ for (let i =0; i < rows; i++) {
 
 function posClick(row, column, event) {
     console.log(`row - ${row} column - ${column} was clicked`);
-    const playedPeice = document.createElement("div");
     document.getElementById("square"+row+column).style = "background-color: yellow;";
 }
 
  
-
-// // A grid position was clicked call the game's turn function, redraw and then check for a winner.
-// function positionClick(row, column, event) {
-//     takeTurn(row, column);
-//     const board = getBoard();
-//     if (!isValidRowOrColumn(board) || !board.every(isValidColumn)) {
-//         throw "Expecting 'getBoard' to return a 2d array where all values match are null or one of the strings 'nought' or 'cross'. Actually received: " + JSON.stringify(board);
-//     }
-//     drawBoard(board);
-//     const winner = checkWinner();
-//     if (winner) {
-//         if (typeof winner !== "string" || !["noughts", "crosses", "nobody"].includes(winner)) {
-//             throw "Expecting 'checkWinner' to return null or one of the strings 'noughts', 'crosses' or 'nobody'. Actually received: " + winner;
-//         }
-//         const winnerName = document.getElementById("winner-name");
-//         winnerName.innerText = winner;
-//         const winnerDisplay = document.getElementById("winner-display");
-//         winnerDisplay.style.display = "block";
-//     }
-// }
