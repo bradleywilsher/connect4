29a45b71c855836cb2193d828c554391744bc770
brad <bradley.wilsher@infinityworks.com>
2020-09-25 10:13:30 +0100

    final cleanup

--- Source/node/gameLogic.js
+++ Source/node/gameLogic.js
@@ -5,25 +5,21 @@ const fs = require('fs').promises
 const fsCheck = require('fs');
 //.promises;
 
-
-
-
 const P1 = "p1"
 const P2 = "p2"
-
 const rows = 4;
 const cols = 5;
 const WINNUMBER = 4;
 const NOWIN = "noWin";
 
-let board = Array(cols).fill().map(() => Array(rows));
+let localBoard = Array(cols).fill().map(() => Array(rows));
 let state = {
     p1Turn: true,
     pieceRow: 0,
     winner: NOWIN,
     //Scores: player1, player2
     playerScores: [0, 0],
-    board
+    board: localBoard
 }
 
 initGame();
@@ -31,7 +27,7 @@ function boardInit() {
     //init array to null
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            board[i][j] = "empty";
+            localBoard[i][j] = "empty";
         }
     }
 }
@@ -39,6 +35,51 @@ function boardInit() {
 function initGame() {
     boardInit();
     getScore();
+}
+
+const app = express();
+const cors = require("cors");
+app.use(express.static("../client"))
+console.log(__dirname)
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+    res.json("world");
+});
+
+app.get("/game/board/restart", (req, res) => {
+    initGame();
+    res.send("Board reset");
+});
+
+app.post('/game/board/col', (req, res) => {
+    res.json(updateBoard(req.body.column, req.body.row, localBoard));
+});
+
+app.post('/game/findPlace', (req, res) => {
+    res.json(findPlace(req.body.col));
+});
+
+app.get('/game/getState/', (req, res) => {
+    res.json(state);
+});
+
+function findPlace(column) {
+    let row = rows - 1;
+    for (let i = row; i >= 0; i--) {
+        if (localBoard[column][i] === "empty") {
+            state.winner = checkWin(column, i, updateBoard(column, i, localBoard));
+
+            //read score
+            state.playerScores = incScore(state.winner, state.playerScores)
+            updateScore(state.playerScores);
+            state.pieceRow = i;
+            state.p1Turn = !(state.p1Turn);
+            return state;
+        }
+    }
 }
 
 async function getScore() {
@@ -66,73 +107,17 @@ function takeTurn(curTurn) {
     return !curTurn;
 }
 
-//funciton that returns an app
-const app = express();
-// eslint-disable-next-line no-undef
-const cors = require("cors");
-const { raw } = require("express");
-
-//gives all my client stuff to server
-app.use(express.static("../client"))
-console.log(__dirname)
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/hello", (req, res) => {
-    res.json("world");
-});
-
-app.get("/game/board/restart", (req, res) => {
-    initGame();
-    res.send("Board reset");
-});
-
-app.post('/game/board/col', (req, res) => {
-    res.json(updateBoard(req.body.column, req.body.row, board));
-});
-
-app.post('/game/findPlace', (req, res) => {
-    res.json(findPlace(req.body.col));
-});
-
-app.get('/game/getState/', (req, res) => {
-    res.json(state);
-});
-
-
-//res.json sends the response back
-
-function findPlace(column) {
-    let row = rows - 1;
-    for (let i = row; i >= 0; i--) {
-        if (board[column][i] === "empty") {
-            state.winner = checkWin(column, i, updateBoard(column, i, board));
-
-            //read score
-            state.playerScores = incScore(state.winner, state.playerScores)
-            updateScore(state.playerScores);
-            state.pieceRow = i;
-            state.p1Turn = !(state.p1Turn);
-            return state;
-        }
-    }
-}
-
 async function updateScore(scores) {
     await fs.writeFile("../../Data/scores.json",
      JSON.stringify(scores), 'utf-8');
 }
 
-async function readScore() {
-}
-
 //Pure check winner 
-//TODO check more efficently + other win conditions
+//TODO check more efficently
 function checkWin(col, row, localBoard) {
-    //Adjust number of rols/cols for the array length
     let p1Score = 0;
     let p2Score = 0;
+    
     //Horizontal Win
     for (let i = 0; i < cols; i++) {
         if (localBoard[i][row] === P1) {
@@ -152,6 +137,80 @@ function checkWin(col, row, localBoard) {
             }
         }
     }
+
+    p1Score =0;
+    p2Score =0;
+
+    //vertical
+    for (let i = 0; i < rows; i++) {
+        if (localBoard[col][i] === P1) {
+            p1Score++;
+            if (p1Score >= WINNUMBER) {
+                return P1
+            }
+        } else {
+            p1Score = 0;
+            if (localBoard[col][i] === P2) {
+                p2Score++;
+                if (p2Score >= WINNUMBER) {
+                    return P2
+                }
+            } else {
+                p2Score = 0;
+            }
+        }
+    }
+
+  
+  //Diagnol win - courtesy of Connor Airid :) 
+  let count = 0;
+  const diagBoard = [];
+  localBoard.flat().forEach((circle) => {
+    const column = count % localBoard[0].length;
+    if (count <localBoard[0].length) {
+      diagBoard.push([]);
+    }
+    diagBoard[column].unshift(`${circle}`);
+    count += 1;
+  });
+
+  for (let row = 0; row < diagBoard.length - WINNUMBER + 1; row += 1) {
+    let leftCounter = 0;
+    let rightCounter = diagBoard[0].length - 1;
+    // loop over columns
+    for (let column = 0; column < diagBoard[0].length - WINNUMBER + 1; column += 1) {
+      // if a colour is encountered
+      if (diagBoard[row][leftCounter] !== "empty") {
+        winner = diagBoard[row][leftCounter];
+        for (let i = 0; i < WINNUMBER; i += 1) {
+          if (i === WINNUMBER - 1) {
+            return winner;
+          }
+          // if there is WINNUMBER in a diagonal down to the right return the winner
+          if (diagBoard[row + i][leftCounter + i] !== diagBoard[row + i + 1][leftCounter + i + 1]) {
+            break;
+          }
+        }
+      }
+      // if a colour is encountered
+      if (diagBoard[row][rightCounter] !== "empty") {
+        winner = diagBoard[row][rightCounter];
+        for (let i = 0; i < WINNUMBER; i += 1) {
+          if (i === WINNUMBER - 1) {
+            return winner;
+          }
+          // if there is WINNUMBER in a diagonal down to the left return the winner
+          if (diagBoard[row + i][rightCounter - i] !== diagBoard[row + i + 1][rightCounter - i - 1]) {
+            break;
+          }
+        }
+      }
+      // increment counters to test for diagonals to the left and right
+      leftCounter += 1;
+      rightCounter -= 1;
+    }
+  }
+
     //if no winner
     return NOWIN;
 }
